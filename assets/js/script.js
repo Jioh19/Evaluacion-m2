0bdd61bf803320eba4b24a2f47e80d7f8a621657
Jioh19 <jioh1982@gmail.com>
2023-04-04 09:33:34 -0400

    Inclusion de modal y jQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,88 +10,121 @@ function reserva() {
     document.getElementById("reserva").setAttribute("class", "formulario py-1 my-4");
 }
 
-//Agregue un event listener. No se si este sera el metodo utilizado en general, pero logro su cometido.
-//Ahora bien no se si estara correcto la verificacion de los datos. Solo chequee de que estos existan
 
-let comentario = document.getElementById("comentario");
+//Me di cuenta de que es pesima idea tener el mismo nombre de id por lo que modifique un poco los ids. 
+//Quizas era mejor reformular mejor el select para que de alguna manera incluyera a los DOM padres
+//Tambien probando por primera vez el try err catch
+$(document).ready(function () {
 
-comentario.addEventListener("submit", (e) => {
-    e.preventDefault();
+    $("#btnReserva").click(function () {
 
-    let nombre = document.getElementById("nombre").value;
-    if (!nombre) {
-        alert("El campo 'Nombre' es obligatorio");
-        return false;
-    }
-    let correo = document.getElementById("correo").value;
-    if (!correo) {
-        alert("El campo 'Correo' es obligatorio");
-        return false;
-    }
-    let telefono = document.getElementById("telefono").value;
-    if (!telefono) {
-        alert("El campo 'Teléfono' es obligatorio");
-        return false;
-    }
-    let texto = document.getElementById("texto").value;
-    if (!texto) {
-        alert("El campo 'Texto' es obligatorio");
-        return false;
-    }
+        let nombre = $("#rnombre").val();
+        try {
+            if (!nombre) throw alert("El campo 'Nombre' es obligatorio");
+        }
+        catch (err) {
+            return false;
+        }
 
-    alert("Muchas gracias " + nombre +", hemos recibido tu sugerencia y enviaremos una pronta respuesta al correo " + 
-    correo);
-});
+        let correo = $("#rcorreo").val();
+        try {
+            if (!correo) throw alert("El campo 'Correo' es obligatorio");
+        }
+        catch (err) {
+            return false;
+        }
 
+        let telefono = $("#rtelefono").val();
+        try {
+            if (!telefono) throw alert("El campo 'Telefono' es obligatorio");
+        }
+        catch (err) {
+            return false;
+        }
 
-//Creo que este metodo es mejor ya que define explicitamente de que formulario se saca la infomracion
-let reservar = document.getElementById("reservar");
+        let fecha = $("#rfecha").val();
+        try {
+            if (!fecha) throw alert("El campo 'Fecha' es obligatorio");
+        }
+        catch (err) {
+            return false;
+        }
 
-reservar.addEventListener("submit", (e) => {
-    e.preventDefault();
- 
-    let nombre = document.forms["reservar"]["nombre"].value;
-    if (!nombre) {
-    alert("El campo 'Nombre' es obligatorio");
-    return false;
-    }
-    //console.log(nombre);
-    
-    let correo = document.forms["reservar"]["correo"].value;
-    if (!correo) {
-        alert("El campo 'Correo' es obligatorio");
-        return false;
-    }
-    //console.log(correo);
+        let hora = $("#rhora").val();
+        try {
+            if (!hora) throw alert("El campo 'Hora' es obligatorio");
+        }
+        catch (err) {
+            return false;
+        }
 
-    let telefono = document.forms["reservar"]["telefono"].value;
-    if (!telefono) {
-        alert("El campo 'Teléfono' es obligatorio");
-        return false;
-    }
-    //console.log(telefono);
+        let asistentes = $("#rasistentes").val();
+        try {
+            if (!asistentes) throw alert("El campo 'Asistentes' es obligatorio");
+        }
+        catch (err) {
+            return false;
+        }
 
-    let fecha = document.forms["reservar"]["fecha"].value;
-    if (!texto) {
-        alert("El campo 'fecha' es obligatorio");
-        return false;
-    }
-    //console.log(fecha);
+        alert("Estimado " + nombre + ", agradecemos por reservar con nosotros. Hemos registrado " +
+            asistentes + " asistentes. Se ha enviado el código de confirmación al correo " + correo +
+            "\nGracias por preferirnos");
+    })
 
-    let hora = document.forms["reservar"]["hora"].value;
-    if (!texto) {
-        alert("El campo 'hora' es obligatorio");
-        return false;
-    }
-    //console.log(hora);
+    //Aprendiendo a usar "Modal". No se como hacer que la pagina principal se mueva de forma independiente al
+    //modal.
+    $(".card").click(function () {
 
-    let asistentes = document.forms["reservar"]["asistentes"].value;
-    if (!texto) {
-        alert("El campo 'asistentes' es obligatorio");
-        return false;
-    }
-    //console.log(asistentes);
+        let titulo = $(this).children(".card-title").text();
+        let imagen = $(this).children(".card-img-top").attr("src");
+        let texto = $(this).find(".card-text").text();
 
-    alert("Muchas gracias " + nombre +", hemos recibido tu reserva y enviaremos una pronta confirmacion al correo " + 
-    correo);
+        console.log(texto);
+
+        $(".modal-title").text(titulo);
+        $(".modal-img").attr("src", imagen);
+        $(".modal-img").css("width", "70%");
+        $(".modal-text").text(texto);
+        $("#myModal").modal('show');
+
+    })
+
+    //Agregue un event listener. No se si este sera el metodo utilizado en general, pero logro su cometido.
+    //Ahora bien no se si estara correcto la verificacion de los datos. Solo chequee de que estos existan
+
+    let comentario = document.getElementById("comentario");
+
+    comentario.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        let nombre = document.forms["comentario"]["nombre"].value;
+        if (!nombre) {
+            alert("El campo 'Nombre' es obligatorio");
+            return false;
+        }
+        //console.log(nombre);
+
+        let correo = document.forms["comentario"]["correo"].value;
+        if (!correo) {
+            alert("El campo 'Correo' es obligatorio");
+            return false;
+        }
+        //console.log(correo);
+
+        let telefono = document.forms["comentario"]["telefono"].value;
+        if (!telefono) {
+            alert("El campo 'Teléfono' es obligatorio");
+            return false;
+        }
+        //console.log(telefono);
+
+        let texto = document.forms["comentario"]["texto"].value;
+        if (!texto) {
+            alert("El campo 'Texto' es obligatorio");
+            return false;
+        }
+
+        alert("Muchas gracias " + nombre + ", hemos recibido tu sugerencia y enviaremos una pronta respuesta al correo " +
+            correo);
+    });
 });
